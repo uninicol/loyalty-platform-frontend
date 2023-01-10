eb3317459a294ae5dcbe1592b9df199dca50f256
Nicol Buratti <nickibrt8@gmail.com>
2023-01-10 11:12:28 +0100

    premuto il bottone si colora di blu

--- src/app/tab-home/categories-filter-buttons/categories-filter-buttons.component.ts
+++ src/app/tab-home/categories-filter-buttons/categories-filter-buttons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-categories-filter-buttons',
@@ -9,13 +9,26 @@ export class CategoriesFilterButtonsComponent implements OnInit {
   categories = ["Tutto", "Moda", "Tecnologia", "Bellezza", "Fitness e salute", "Lifestyle", "Sport", "Istruzione"]
   currentCategory = this.categories[0];
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {}
+  }
 
-  handleRefresh(event: any) {
+  ngOnInit() {
+
+  }
+
+  handleRefresh(event: any, category: string) {
     setTimeout(() => {
       // Any calls to load data go here
+      //refresh pagina con la categoria
+
+      // @ts-ignore
+      document.getElementById(category)
+        .setAttribute("fill", "solid")
+      // @ts-ignore
+      document.getElementById(this.currentCategory)
+        .setAttribute("fill", "outline")
+      this.currentCategory = category
       event.target.complete();
     }, 2000);
   }
