cc4986ea82e4a173dba406a4049552e6ce853ddb
Nicol Buratti <nickibrt8@gmail.com>
2023-01-18 18:55:19 +0100

    implementazione base autenticazione JWT

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+import {BehaviorSubject, Observable} from 'rxjs';
+
+import {Storage} from '@ionic/storage';
+import {User} from './user';
+import {AuthResponse} from './auth-response';
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  authSubject = new BehaviorSubject(false);
+
+  constructor(private httpClient: HttpClient,
+              private storage: Storage) {
+  }
+
+  register(user: User): Observable<AuthResponse> {
+    return this.httpClient.post<AuthResponse>(`${environment.apiUrl}/register`, user).pipe(
+      tap(async (res: AuthResponse) => {
+
+        if (res.user) {
+          await this.storage.set("ACCESS_TOKEN", res.user.access_token);
+          await this.storage.set("EXPIRES_IN", res.user.expires_in);
+          this.authSubject.next(true);
+        }
+      })
+    );
+  }
+
+  login(user: User): Observable<AuthResponse> {
+    return this.httpClient.post<AuthResponse>(`${environment.apiUrl}/login`, user).pipe(
+      tap(async (res: AuthResponse) => {
+
+        if (res.user) {
+          await this.storage.set("ACCESS_TOKEN", res.user.access_token);
+          await this.storage.set("EXPIRES_IN", res.user.expires_in);
+          this.authSubject.next(true);
+        }
+      })
+    );
+  }
+
+  async logout() {
+    await this.storage.remove("ACCESS_TOKEN");
+    await this.storage.remove("EXPIRES_IN");
+    this.authSubject.next(false);
+  }
+
+  isLoggedIn() {
+    return this.authSubject.asObservable();
+  }
+}
